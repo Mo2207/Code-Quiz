262021de8fa74ea02bab7979fa15290edbc7ff7d
Gabe Morris <gabemorris1042@gmail.com>
2022-11-10 17:06:01 -0400

    inital commit, basic html layout

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+
+// ----- Answers ------
+let ans1 = document.getElementById("answer1");
+let ans2 = document.getElementById("answer2");
+let ans3 = document.getElementById("answer3");
+let ans4 = document.getElementById("answer3");
+
